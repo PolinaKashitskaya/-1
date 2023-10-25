b9b1245a6144f2f6596e80a85409759b0bc642c1
PolinaKashitskaya <147952926+PolinaKashitskaya@users.noreply.github.com>
2023-10-25 03:22:14 +0300

    Update 2. Продолжение финансового планирования.py

--- Лабораторная работа 2/2. Продолжение финансового планирования.py
+++ Лабораторная работа 2/2. Продолжение финансового планирования.py
@@ -3,15 +3,15 @@ spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
 
-# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
-
-stop = 10
+money_capital = 0
+stop = 9
 
 for i in range(stop):
-    amount_of_money_per_month = ((spend + (spend * increase)) - salary)
-    print(amount_of_money_per_month)
+    percent = spend * increase
+    spend += percent
+    amount_of_money_per_month: int = spend - salary
+    money_capital += amount_of_money_per_month
 
-money_capital = sum(amount_of_money_per_month) + 1000
-#1000 за первый месяц без процентов
+money_capital = round(money_capital) + 1000  # 1000 за первый месяц без процентов
 
 print("Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
